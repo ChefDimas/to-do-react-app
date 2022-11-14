bbc425d73e4a961b84b240fa714d6711d8c3b7ee
ChefDimas <dimalunik@aol.com>
2022-11-14 20:49:55 +0100

    project set-up

---
+++ src/components/screens/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const todos = [
+    {
+        title: 'Finish the essay',
+        isCompleted: false,
+    },
+    {
+        title: 'Read next chapter',
+        isCompleted: false,
+    },
+    {
+        title: 'Do the math for tomorrow',
+        isCompleted: false,
+    }
+]
+
+const Home = () => {
+    return (
+        <div className="bg-gray-900 h-screen text-white">
+
+        </div>
+    )
+};
+
+export default Home;
