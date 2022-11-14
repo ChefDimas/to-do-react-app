b7b038cf69ea9590416fc0ea3a57d7736719fc24
ChefDimas <dimalunik@aol.com>
2022-11-14 21:07:58 +0100

    Installed react-icons, deleting icons from manifest, update TodoItem.jsx and creating CheckBox.jsx

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import TodoItem from "./item/TodoItem";
 
 const todos = [
     {
+        _id: 1,
         title: 'Finish the essay',
         isCompleted: false,
     },
     {
+        _id: 2,
         title: 'Read next chapter',
         isCompleted: false,
     },
     {
+        _id: 3,
         title: 'Do the math for tomorrow',
         isCompleted: false,
     }
@@ -18,7 +22,12 @@ const todos = [
 const Home = () => {
     return (
         <div className="bg-gray-900 h-screen text-white">
-
+            {todos.map(todo =>
+            <TodoItem
+            todo={todo}
+            key={todo._id}
+            />
+            )}
         </div>
     )
 };
